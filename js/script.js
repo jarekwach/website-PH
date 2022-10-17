3bb72ecdd1875840961f0f127f53fb00d0dc1b35
Jarosław Wąchała <jarek@MacBook-Air-Jarosaw.local>
2022-10-17 13:02:53 +0200

    fix: update login function

--- js/script.js
+++ js/script.js
@@ -2,11 +2,18 @@ const loginMsg = document.querySelector('.login-msg')
 const registerMsg = document.querySelector('.register-msg')
 const loginForm = document.querySelector('.login-form')
 const registerForm = document.querySelector('.register-form')
+const navLoginLink = document.querySelector('.nav-login-link')
 
 function changeForm() {
 	loginForm.classList.toggle('disabled')
 	registerForm.classList.toggle('disabled')
 }
 
+function showLoginForm() {
+	loginForm.classList.remove('disabled')
+	registerForm.classList.add('disabled')
+}
+
 loginMsg.addEventListener('click', changeForm)
 registerMsg.addEventListener('click', changeForm)
+navLoginLink.addEventListener('click', showLoginForm)
