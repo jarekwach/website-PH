e7c9a4b4ca9f4b99b7e104b726183cd6af8fd890
Jarosław Wąchała <jarek@MacBook-Air-Jarosaw.local>
2022-10-19 13:02:39 +0200

    feat: add fetch data from a json file

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ const loginForm = document.querySelector('.login-form')
 const registerForm = document.querySelector('.register-form')
 const navLoginLink = document.querySelector('.nav-login-link')
 
+const productTitle = document.querySelector('.product-title')
+const dateAdd = document.querySelector('.product-item-date')
+const itemDescription = document.querySelector('.product-item-description')
+const productImg = document.querySelector('.item-photo')
+
 function changeForm() {
 	loginForm.classList.toggle('disabled')
 	registerForm.classList.toggle('disabled')
@@ -13,6 +18,16 @@ function showLoginForm() {
 	loginForm.classList.remove('disabled')
 	registerForm.classList.add('disabled')
 }
+
+fetch('./items.json')
+	.then((res) => res.json())
+	.then(function (data) {
+		productTitle.textContent = data.items[0].title
+		itemDescription.textContent = data.items[0].description
+		dateAdd.textContent = data.items[0].dateAdded
+		productImg.setAttribute('src', data.items[0].photo)
+	})
+	.catch((err) => console.error(err))
 
 loginMsg.addEventListener('click', changeForm)
 registerMsg.addEventListener('click', changeForm)
