f4e49e34628dd1af15930ab8c5ab3cd2e60d5927
Jarosław Wąchała <jarek@MacBook-Air-Jarosaw.local>
2022-10-25 19:36:28 +0200

    feat: add voting function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
 import { getProducts } from './get-products.js'
 import { navMobile } from './nav.js'
 
-const productTitle = document.querySelector('.product-title')
-const dateAdd = document.querySelector('.product-item-date')
-const itemDescription = document.querySelector('.product-item-description')
-const productImg = document.querySelector('.item-photo')
-const allProductBox = document.querySelector('.all-products-box')
+// const productTitle = document.querySelector('.product-title')
+// const dateAdd = document.querySelector('.product-item-date')
+// const itemDescription = document.querySelector('.product-item-description')
+// const productImg = document.querySelector('.item-photo')
+// const allProductBox = document.querySelector('.all-products-box')
 
-const voteBtn = document.querySelector('.vote-btn')
-const numbersOfVote = document.querySelector('.number-of-votes')
+// console.log(document.querySelector('.js-vote'))
+document.querySelector('.js-products').innerHTML = getProducts.join('')
+// console.log(document.querySelector('.js-vote'))
+
+// const addVote = () => {
+// 	numVote += 1
+// 	numbersOfVote.textContent = numVote
+// }
+
+// const jsBtnVote = document.querySelectorAll('.js-vote')
+
+// voteBtn?.addEventListener('click', addVote)
+
+const allVoteBtn = document.querySelectorAll('.js-vote')
 let numVote = 0
 
-const addVote = () => {
-	numVote += 1
-	numbersOfVote.textContent = numVote
-}
-
-voteBtn?.addEventListener('click', addVote)
-
-console.log(document.querySelector('.js-vote'))
-document.querySelector('.js-products').innerHTML = getProducts.join('')
-console.log(document.querySelector('.js-vote'))
+allVoteBtn.forEach((el) =>
+	el.addEventListener('click', (e) => {
+		const parent = e.target.closest('.btn-box')
+		
+		numVote += 1
+		parent.querySelector('.number-of-votes').textContent = numVote
+	})
+)
