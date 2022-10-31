d0b63f53dcb98140255b3ed0cd03dfcaee57d9b8
Jarosław Wąchała <jarek@MacBook-Air-Jarosaw.local>
2022-10-31 14:44:51 +0100

    fix: update function handleNav

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,12 @@
 const navMobileList = document.querySelector('.nav-mobile-list')
 const allHideLink = document.querySelectorAll('.nav-mobile-list a')
-const burgerBtn = document.querySelector('.burger-ico')
 
-const handleNav = () => {
+export function handleNav() {
 	navMobileList.classList.toggle('active-nav-link')
-
-	allHideLink.forEach((item) =>
-		item.addEventListener('click', () =>
-			navMobileList.classList.remove('active-nav-link')
-		)
-	)
 }
 
-export const navMobile = burgerBtn.addEventListener('click', handleNav)
+allHideLink.forEach((item) =>
+	item.addEventListener('click', () =>
+		navMobileList.classList.remove('active-nav-link')
+	)
+)
--- js/script.js
+++ js/script.js
@@ -1,33 +1,7 @@
 import { getProducts } from './get-products.js'
-import { navMobile } from './nav.js'
+import { handleNav } from './nav.js'
+import { addVotes } from './voting.js'
 
-// const productTitle = document.querySelector('.product-title')
-// const dateAdd = document.querySelector('.product-item-date')
-// const itemDescription = document.querySelector('.product-item-description')
-// const productImg = document.querySelector('.item-photo')
-// const allProductBox = document.querySelector('.all-products-box')
-
-// console.log(document.querySelector('.js-vote'))
+document.querySelector('.burger-ico').addEventListener('click', handleNav)
 document.querySelector('.js-products').innerHTML = getProducts.join('')
-// console.log(document.querySelector('.js-vote'))
-
-// const addVote = () => {
-// 	numVote += 1
-// 	numbersOfVote.textContent = numVote
-// }
-
-// const jsBtnVote = document.querySelectorAll('.js-vote')
-
-// voteBtn?.addEventListener('click', addVote)
-
-const allVoteBtn = document.querySelectorAll('.js-vote')
-let numVote = 0
-
-allVoteBtn.forEach((el) =>
-	el.addEventListener('click', (e) => {
-		const parent = e.target.closest('.btn-box')
-		
-		numVote += 1
-		parent.querySelector('.number-of-votes').textContent = numVote
-	})
-)
+addVotes('.js-vote', '.number-of-votes')
